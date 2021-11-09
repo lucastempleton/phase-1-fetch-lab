118d9e490ea54e6bc6ef60bb18f132ef472e7400
Lucas Templeton <templetl99@gmail.com>
2021-11-08 21:44:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => res.json())
+  .then((books) => renderBooks(books))
 }
 
 function renderBooks(books) {
@@ -9,9 +11,10 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', () => {
   fetchBooks();
-});
+})
